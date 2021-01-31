30864555801430f421d49f8c33dbc11d88a8060e
KrUtYaIII <burdunin.gosha@yandex.ru>
2021-01-31 18:38:58 +0300

    Restarting

--- Source/MyProject9/Private/BP_PlayerController.cpp
+++ Source/MyProject9/Private/BP_PlayerController.cpp
@@ -88,6 +88,11 @@ void ABP_PlayerController::ChangePlayersVoteCount_Implementation()
 	{
 		if ((GS->AgreedPlayers + 1) == GS->AmountOfPlayers)
 		{
+			TArray<AActor*>PlayerControllers;
+			UGameplayStatics::GetAllActorsOfClass(GetWorld(), ABP_PlayerController::StaticClass(), PlayerControllers);
+			for (auto PC : PlayerControllers) {
+				Cast<ABP_PlayerController>(PC)->RemoveFinalMenu();
+			}
 			GS->ResetGame();
 		}
 		else {
@@ -119,8 +124,10 @@ bool ABP_PlayerController::ChangePlayersVoteCount_Validate()
 void ABP_PlayerController::RemoveFinalMenu_Implementation()
 {
 	bShowMouseCursor = false;
-	FinalMenu->TB_Restart->SetVisibility(ESlateVisibility::Visible);
-	FinalMenu->Throbbler->SetVisibility(ESlateVisibility::Visible);
-	FinalMenu->BTN_PlayMore->SetVisibility(ESlateVisibility::Hidden);
-	FinalMenu->BTN_Exit->SetVisibility(ESlateVisibility::Hidden);
+	if (FinalMenu) {
+		FinalMenu->TB_Restart->SetVisibility(ESlateVisibility::Visible);
+		FinalMenu->Throbbler->SetVisibility(ESlateVisibility::Visible);
+		FinalMenu->BTN_PlayMore->SetVisibility(ESlateVisibility::Hidden);
+		FinalMenu->BTN_Exit->SetVisibility(ESlateVisibility::Hidden);
+	}
 }
